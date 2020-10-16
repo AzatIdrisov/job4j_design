16c10e0c1a5c858736852b88f31990ed26a611d8
Azat Idrisov <azzzat72@yandex.ru>
2020-10-16 13:18:58 +0500

    2. Анализ доступности сервера. [#348760]

--- сhapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ сhapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -5,39 +5,43 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<String> lines = new ArrayList<String>();
+        List<String> result = new ArrayList<String>();
+        String startTime = "", lastTime;
         try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-            read.lines().forEach(lines::add);
+            String line = read.readLine();
+            while (line != null) {
+                System.out.println(line);
+                    if (line.equals("")) {
+                        line = read.readLine();
+                        continue;
+                    }
+                    if (Integer.parseInt(line.substring(0, 3)) >= 400 && startTime.equals("")) {
+                        startTime = line.substring(4);
+                    }
+                    if (Integer.parseInt(line.substring(0, 3)) < 400 && !startTime.equals("")) {
+                        lastTime = line.substring(4);
+                        result.add(startTime + ";" + lastTime + ";");
+                        startTime = "";
+                    }
+                line = read.readLine();
+            }
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        List<String> result = new ArrayList<String>();
-        Iterator<String> iterator = lines.iterator();
-        String temp, startTime = "", lastTime;
-        while (iterator.hasNext()) {
-            temp = iterator.next();
-            if (temp.equals("")) {
-                continue;
-            }
-            if (Integer.parseInt(temp.substring(0, 3)) >= 400 && startTime.equals("")) {
-                startTime = temp.substring(4);
-            }
-            if (Integer.parseInt(temp.substring(0, 3)) < 400 && !startTime.equals("")) {
-                lastTime = temp.substring(4);
-                result.add(startTime + ";" + lastTime + ";");
-                startTime = "";
-            }
         }
         try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
             result.stream().forEach(el -> out.println(el));
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private boolean checkError(String logLine) {
+        boolean result = true;
+        return result;
     }
 
     public static void main(String[] args) {
