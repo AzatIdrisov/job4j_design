d0de050a917e81b6ddef66cbd0407f8cff0d0c4c
Azat Idrisov <azzzat72@yandex.ru>
2020-12-14 23:03:15 +0500

    Фильтры [#348688]

--- select.sql
+++ select.sql
@@ -1,8 +1,8 @@
 select * from product where  type_id = (select id from type where name ='СЫР');
 select * from product where  name like '%мороженное%';
-select * from product where extract(month from expired_date) = extract(month from current_date)+1;
+select name from product where  expired_date between current_date and current_date + interval '1 month';
 select * from product where price = (select max(price) from product);
 select count(type_id) from product where type_id = 5;
-select * from product where type_id in ( (select id from type where name ='СЫР'), (select id from type where name ='МОЛОКО'));
-select name from type where (select count(a.id) from product as a where a.type_id = type.id) < 10;
+select * from product where type_id in ( select id from type where name ='СЫР' or name ='МОЛОКО');
+select type.name from type join product p on type.id = p.type_id group by type.name having count(p.type_id) < 10;
 select prod.name, prod_type.name from product as prod left join type as prod_type on prod_type.id = prod.type_id;
